aa0b409cbe5e4e6c8bfc03ad863afcf74d73ef11
Mattia Salnitri <mattia.salnitri@polimi.it>
2019-02-22 15:16:42 +0100

    fixed version for image pull

--- jenkins/deploy-staging.sh
+++ jenkins/deploy-staging.sh
@@ -7,6 +7,6 @@ ssh -i /opt/keypairs/ditas-testbed-keypair.pem cloudsigma@31.171.247.162 << 'END
 # Ensure that a previously running instance is stopped (-f stops and removes in a single step)
 # || true - "docker stop" failt with exit status 1 if image doen't exists, what makes the Pipeline fail. the "|| true" forces the command to exit with 0.
 sudo docker rm -f decision-system-for-data-and-computation-movement || true
-sudo docker pull ditas/decision-system-for-data-and-computation-movement:latest
+sudo docker pull ditas/decision-system-for-data-and-computation-movement:staging
 sudo docker run -p 30003:30003 --restart unless-stopped -d --name decision-system-for-data-and-computation-movement ditas/decision-system-for-data-and-computation-movement:staging
 ENDSSH
