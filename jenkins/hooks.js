1330a8105f003dc0707f9a8e34501ba209fe8026
Mattia Salnitri <mattia.salnitri@polimi.it>
2019-03-22 19:15:05 +0100

    change path hooks

--- jenkins/hooks.js
+++ jenkins/hooks.js
@@ -18,8 +18,7 @@ before("/NotifyViolation > POST > 200", function (transaction) {
 	
 	//transaction.request.headers['content-type'] = "application/json"
 	//transaction.request.headers['Violations'] = "[ { \"vdcId\": \"1\", \"methodId\": \"getAllValuesForBloodTestComponent\", \"metrics\": [ { \"key\": \"availability\", \"value\": 90, \"datetime\": \"2018-08-07T10:50:23.674337517+02:00\"}]}]"
-	//transaction.test.request.body = "prova notifyViolation";
-	transaction.test.actual.body = 'test NotifyVdc';
+	transaction.request.body  = "prova notifyViolation";
 	done();
 });
 
@@ -29,8 +28,7 @@ before("/AddVDC > POST > 200", function (transaction) {
 	
 	//transaction.request.headers['content-type'] = "application/json"
 	//transaction.request.headers['Violations'] = "[ { \"vdcId\": \"1\", \"methodId\": \"getAllValuesForBloodTestComponent\", \"metrics\": [ { \"key\": \"availability\", \"value\": 90, \"datetime\": \"2018-08-07T10:50:23.674337517+02:00\"}]}]"
-	//transaction.test.request.body = "prova add VDC";
-	transaction.test.actual.body = 'test addVdc';
+	transaction.request.body = "prova add VDC";
 	done();
 });
 
