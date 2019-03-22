176a2a086560800750704df7bf7e54f233f0210a
Mattia Salnitri <mattia.salnitri@polimi.it>
2019-03-22 18:53:04 +0100

    added done(); in hooks

--- jenkins/hooks.js
+++ jenkins/hooks.js
@@ -18,7 +18,8 @@ before("/NotifyViolation > POST > 200", function (transaction) {
 	
 	//transaction.request.headers['content-type'] = "application/json"
 	//transaction.request.headers['Violations'] = "[ { \"vdcId\": \"1\", \"methodId\": \"getAllValuesForBloodTestComponent\", \"metrics\": [ { \"key\": \"availability\", \"value\": 90, \"datetime\": \"2018-08-07T10:50:23.674337517+02:00\"}]}]"
-	transaction.request.body = "prova notifyViolation"
+	transaction.request.body = "prova notifyViolation";
+	done();
 });
 
 
@@ -27,6 +28,7 @@ before("/AddVDC > POST > 200", function (transaction) {
 	
 	//transaction.request.headers['content-type'] = "application/json"
 	//transaction.request.headers['Violations'] = "[ { \"vdcId\": \"1\", \"methodId\": \"getAllValuesForBloodTestComponent\", \"metrics\": [ { \"key\": \"availability\", \"value\": 90, \"datetime\": \"2018-08-07T10:50:23.674337517+02:00\"}]}]"
-	transaction.request.body = "prova add VDC"
+	transaction.request.body = "prova add VDC";
+	done();
 });
 
