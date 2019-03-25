0b104850870e07387a449f3eb68d08f15446df26
Mattia Salnitri <mattia.salnitri@polimi.it>
2019-03-25 09:54:22 +0100

    fixed calls hooks

--- jenkins/hooks.js
+++ jenkins/hooks.js
@@ -14,7 +14,7 @@ before("/AddVDC > POST > 200", function (transaction) {
 });
 
 //call the notify violations with the same ID of the VDC
-before("NotifyViolation > POST > 200", function (transaction) {
+hooks.before("NotifyViolation > POST > 200", function (transaction) {
 	
 	//transaction.request.headers['content-type'] = "application/json"
 	//transaction.request.headers['Violations'] = "[ { \"vdcId\": \"1\", \"methodId\": \"getAllValuesForBloodTestComponent\", \"metrics\": [ { \"key\": \"availability\", \"value\": 90, \"datetime\": \"2018-08-07T10:50:23.674337517+02:00\"}]}]"
@@ -24,7 +24,7 @@ before("NotifyViolation > POST > 200", function (transaction) {
 
 
 //call the notify violations with the same ID of the VDC
-before("AddVDC > POST > 200", function (transaction) {
+hooks.before("AddVDC > POST > 200", function (transaction) {
 	
 	//transaction.request.headers['content-type'] = "application/json"
 	//transaction.request.headers['Violations'] = "[ { \"vdcId\": \"1\", \"methodId\": \"getAllValuesForBloodTestComponent\", \"metrics\": [ { \"key\": \"availability\", \"value\": 90, \"datetime\": \"2018-08-07T10:50:23.674337517+02:00\"}]}]"
