74bc5940653a912a4cbd852c24cf9ad13bb4f6ed
Mattia Salnitri <mattia.salnitri@polimi.it>
2019-03-25 11:30:00 +0100

    add done() to hooks

--- jenkins/hooks.js
+++ jenkins/hooks.js
@@ -6,14 +6,14 @@ var responseStash = 'prova';
 
 
 // call the addVDC with the blueprint
-before("/AddVDC > POST > 200", function (transaction) {
+before("AddVDC > POST > 200", function (transaction) {
 	transaction.request.headers['content-type'] = "application/json"
 	transaction.request.body = ""	
-  
+	done();
 });
 
 //call the notify violations with the same ID of the VDC
-before("/NotifyViolation > POST > 200", function (transaction) {
+before("NotifyViolation > POST > 200", function (transaction) {
 	
 	transaction.request.headers['content-type'] = "application/json"
 	//transaction.request.headers['Violations'] = "[ { \"vdcId\": \"1\", \"methodId\": \"getAllValuesForBloodTestComponent\", \"metrics\": [ { \"key\": \"availability\", \"value\": 90, \"datetime\": \"2018-08-07T10:50:23.674337517+02:00\"}]}]"
