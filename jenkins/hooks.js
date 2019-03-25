1054a300b7aca6efd91a92eb5db299fec352e8eb
Mattia Salnitri <mattia.salnitri@polimi.it>
2019-03-25 17:17:05 +0100

    modify path to read file hooks

--- jenkins/hooks.js
+++ jenkins/hooks.js
@@ -9,7 +9,7 @@ var fs = require('fs');
 // call the addVDC with the blueprint
 before("/AddVDC > add VDC > 200 > application/json", function (transaction) {
 	//get the json file to send
-	var VDC = fs.readFileSync('../testResources/example_V5_complete_withResourcesMoved.json', 'utf8');
+	var VDC = fs.readFileSync('./testResources/example_V5_complete_withResourcesMoved.json', 'utf8');
 	
 	transaction.request.headers['content-type'] = "text";
 	transaction.request.body = VDC;
@@ -18,7 +18,7 @@ before("/AddVDC > add VDC > 200 > application/json", function (transaction) {
 //call the notify violations with the same ID of the VDC
 before("/NotifyViolation > notifyViolation > 200 > application/json", function (transaction) {
 	//get the json file to send
-	var violations = fs.readFileSync('../testResources/test_violation.json', 'utf8');
+	var violations = fs.readFileSync('./testResources/test_violation.json', 'utf8');
 	
 	transaction.request.headers['content-type'] = "application/json";
 	//transaction.request.headers['Violations'] = "[ { \"vdcId\": \"1\", \"methodId\": \"getAllValuesForBloodTestComponent\", \"metrics\": [ { \"key\": \"availability\", \"value\": 90, \"datetime\": \"2018-08-07T10:50:23.674337517+02:00\"}]}]"
