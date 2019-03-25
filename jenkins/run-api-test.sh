ce8916bcfeec4fd0ef5d61cf135038e819ffa9db
Mattia Salnitri <mattia.salnitri@polimi.it>
2019-03-25 10:52:20 +0100

    fix API description

--- jenkins/run-api-test.sh
+++ jenkins/run-api-test.sh
@@ -6,7 +6,7 @@
 COMPONENT_URL="http://31.171.247.162:30003/"
 
 # TO CHANGE: This line should point to the yaml file of your respository of your api definition
-YAML_FILE="documentation/APISpecification.yaml"
+YAML_FILE="documentation/APISpecification_integrationTest.yaml"
 
 attempts=0
 until $(curl --output /dev/null --silent $COMPONENT_URL); do
